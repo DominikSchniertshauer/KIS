2b073067ba1baf2bb3d26688a6eb9cd77cf5031b
DominikSchniertshauer <d.schniertshauer@gmx.de>
2014-06-17 14:51:25 +0200

    Hospitalization Reiter, Grundlayout festgelegt, Comboboxen an Datenbank gebunden

--- ZY_SS14_T4_SAPUI5_KIS/WebContent/zy_ss14_t4_sapui5_kis/hospitalization.view.js
+++ ZY_SS14_T4_SAPUI5_KIS/WebContent/zy_ss14_t4_sapui5_kis/hospitalization.view.js
@@ -32,27 +32,9 @@ sap.ui.jsview("zy_ss14_t4_sapui5_kis.hospitalization", {
 	   /** 
 	   * Create Buttons:
 	   */
-		var instruct_patient_button = new sap.ui.commons.Button("instruct_patient_create", {
-	        text : "Patient einweisen",
-	        icon : "sap-icon://wounds-doc",
-	        press : function() {open_create_dialog();
-			}
-	    	
-		});
-		var diagnose_patient_button = new sap.ui.commons.Button("diagnose_patient_instruct", {
-	        text : "Diagnose anfertigen",
-	        icon : "sap-icon://wounds-doc",
-	        press : function() {open_create_dialog();
-			}
-	    	
-		});
-		var treatmentplan_patient_button = new sap.ui.commons.Button("treatmentplan_patient_instruct", {
-	        text : "Behandlungsplan anlegen",
-	        icon : "sap-icon://wounds-doc",
-	        press : function() {open_create_dialog();
-			}
-	    	
-		});
+		
+		
+	
 		var bed_patient_button = new sap.ui.commons.Button("bed_patient_instruct", {
 	        text : "Bed zuweisen",
 	        icon : "sap-icon://wounds-doc",
@@ -67,9 +49,82 @@ sap.ui.jsview("zy_ss14_t4_sapui5_kis.hospitalization", {
 			}
 	    	
 		});
+		var oModel = new sap.ui.model.odata.ODataModel( sap.ui.getCore().byId("path").getText(),false);
+
+		var patient_comb_label = new sap.ui.commons.Label({text:"Schritt 1: Patient waehlen/ erstellen" });
+		var patient_comb_temp = new sap.ui.core.ListItem({text:"{Lastname}", additionalText:"{Firstname}"});
+		var patient_comb = new sap.ui.commons.ComboBox({
+		    items: {
+				path: "/PATIENT", 
+				displaySecondaryValues:true,
+				template: patient_comb_temp
+			}
+		});
+		var patient_create_button = new sap.ui.commons.Button({
+	        text : "Patient neu anlegen",
+	        icon : "sap-icon://syringe",
+	        press : function() {open_create_dialog();
+			}
+	    	
+		});
 		
-		layout.createRow(instruct_patient_button, diagnose_patient_button, treatmentplan_patient_button, bed_patient_button, release_patient_button);
-	
+		var conditn_comb_label = new sap.ui.commons.Label({text:"Schritt 2: Diagnose waehlen/ erstellen" });
+		var conditn_comb_temp = new sap.ui.core.ListItem({text:"{Name}", additionalText:"{Description}"});
+		var conditn_comb = new sap.ui.commons.ComboBox({
+		    items: {
+				path: "/CONDITN", 
+				displaySecondaryValues:true,
+				template: conditn_comb_temp
+			}
+		});
+		var conditn_create_button = new sap.ui.commons.Button({
+	        text : "Diagnose neu anlegen",
+	        icon : "sap-icon://electrocardiogram",
+	        press : function() {open_create_dialog();
+			}
+	    	
+		});
+		
+		var treatpl_comb_label = new sap.ui.commons.Label({text:"Schritt 3: Behandlungsplan waehlen/ erstellen" });
+		var treatpl_comb_temp = new sap.ui.core.ListItem({text:"{Name}", additionalText:"{Duration}"});
+		var treatpl_comb = new sap.ui.commons.ComboBox({
+		    items: {
+				path: "/TREATPL", 
+				displaySecondaryValues:true,
+				template: treatpl_comb_temp
+			}
+		});
+		var treatpl_create_button = new sap.ui.commons.Button({
+	        text : "Behandlungsplan neu anlegen",
+	        icon : "sap-icon://clinical-order",
+	        press : function() {open_create_dialog();
+			}
+	    	
+		});
+		
+		var bed_comb_label = new sap.ui.commons.Label({text:"Schritt 4: Bett waehlen/ erstellen" });
+		var bed_comb_temp = new sap.ui.core.ListItem({text:"{Name}", additionalText:"{Duration}"});
+		var bed_comb = new sap.ui.commons.ComboBox({
+		    items: {
+				path: "", 
+				displaySecondaryValues:true,
+				template: bed_comb_temp
+			}
+		});
+		var bed_create_button = new sap.ui.commons.Button({
+	        text : "Bett neu anlegen",
+	        icon : "sap-icon://bed",
+	        press : function() {open_create_dialog();
+			}
+	    	
+		});
+		
+		layout.createRow(patient_comb_label, patient_comb, patient_create_button);
+		layout.createRow(conditn_comb_label, conditn_comb, conditn_create_button);
+		layout.createRow(treatpl_comb_label, treatpl_comb, treatpl_create_button);
+		layout.createRow(bed_comb_label, bed_comb, bed_create_button);		
+
+
 		
 		return layout;
 	}
