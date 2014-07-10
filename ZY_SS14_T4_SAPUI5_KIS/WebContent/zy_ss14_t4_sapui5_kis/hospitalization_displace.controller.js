a7346bb39955e185f722183c07069d42bc29cdc4
Patrick Lanners <mail@patricklanners.lu>
2014-07-10 20:42:26 +0200

    Begonnen mit der Einweisung

--- ZY_SS14_T4_SAPUI5_KIS/WebContent/zy_ss14_t4_sapui5_kis/hospitalization_displace.controller.js
+++ ZY_SS14_T4_SAPUI5_KIS/WebContent/zy_ss14_t4_sapui5_kis/hospitalization_displace.controller.js
@@ -5,85 +5,91 @@ sap.ui.controller("zy_ss14_t4_sapui5_kis.hospitalization_displace", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf zy_ss14_t4_sapui5_kis.hospitalization_displace
 */
-//	onInit: function() {
+//    onInit: function() {
 //
-//	},
+//    },
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
 * @memberOf zy_ss14_t4_sapui5_kis.hospitalization_displace
 */
-//	onBeforeRendering: function() {
+//    onBeforeRendering: function() {
 //
-//	},
+//    },
 
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
 * @memberOf zy_ss14_t4_sapui5_kis.hospitalization_displace
 */
-//	onAfterRendering: function() {
+//    onAfterRendering: function() {
 //
-//	},
+//    },
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 * @memberOf zy_ss14_t4_sapui5_kis.hospitalization_displace
 */
-//	onExit: function() {
+//    onExit: function() {
 //
-//	}
-	
-	get_patient: function(insnr, fields){
-		var oModel = new sap.ui.model.odata.ODataModel( sap.ui.getCore().byId("path").getText(),false);
+//    }
+    
+    get_patient: function(insnr, fields){
+        var oModel = new sap.ui.model.odata.ODataModel( sap.ui.getCore().byId("path").getText(),false);
 
-		var field;
-		var oEntry = {
-		};	
-		
-		if(insnr.length == 10){
-			
-			oModel.read("/PATIENT?$filter=Insurancenumber eq '"+insnr+"'" ,undefined, undefined, true,
-					function(data, response){
-			
-				try {
-					if (data.results[0].PatientID != ''){
-
-						for(var i in fields){
-							field = sap.ui.getCore().byId(fields[i]+"_input");
-							field.setEditable(false);
-						
-							if(fields[i] == 'Fname'){
-								field.setValue(data.results[0].Firstname);
-//								oEntry.Firstname = field.getValue();
-							}	
-						
-							if(fields[i] == 'Lname'){
-								field.setValue(data.results[0].Lastname);
-//								oEntry.Lastname = field.getValue();
-							}
-							
-							oView.patientID = data.results[0].patient_id;
-						}
-
-						tmpPatientID = data.results[0].PatientID;
-						alert(tmpPatientID);
-
-					}
-					
-				} catch(e) {
-					
-				};
-				
-			
-			});
-		}
-	},
-	
-	displace_patient : function (patientID) {
-		
-		alert(patientID);
-	}
+        var field;
+        var oEntry = {
+        };    
+        
+        if(insnr.length == 10){
+        
+            oModel.read("/PATIENT?$filter=Insurancenumber eq '"+insnr+"'" ,undefined, undefined, true,
+                    function(data, response){
+            
+                try {
+                    if (data.results[0].PatientID != ''){
+                        
+        
+                        for(var i in fields){
+                            field = sap.ui.getCore().byId(fields[i]+"_input");
+                            field.setEditable(false);
+                        
+                            if(fields[i] == 'Fname'){
+                                field.setValue(data.results[0].Firstname);
+//                                oEntry.Firstname = field.getValue();
+                            }    
+                        
+                            if(fields[i] == 'Lname'){
+                                field.setValue(data.results[0].Lastname);
+//                                oEntry.Lastname = field.getValue();
+                            }
+                            
+                        
+                        }
+                        window.tmpPatientID= data.results[0].PatientID;
+                    }
+                    
+                } catch(e) {
+                    
+                };
+                
+            });
+            
+           
+        }
+    },
+    
+    displace_patient : function (patientID) {
+        
+    	if (patientID != '') {
+    		var date = new Date();
+    		alert();
+    		
+    	}
+    	
+        
+    }
 
 });
+
